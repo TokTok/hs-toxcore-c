8f2d16c08943824013ded5447e1fe99496567846
iphydf <iphydf@users.noreply.github.com>
2022-02-13 15:17:11 +0000

    chore: Add missing msgpack dependency.

--- tools/prepare_third_party.sh
+++ tools/prepare_third_party.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-sudo apt-get install libopus-dev libsodium-dev libvpx-dev
+sudo apt-get install libmsgpack-dev libopus-dev libsodium-dev libvpx-dev
 
 git clone --depth=1 https://github.com/TokTok/c-toxcore
 cmake -Bc-toxcore/_build -Hc-toxcore -DBOOTSTRAP_DAEMON=OFF -DCMAKE_INSTALL_PREFIX:PATH="/usr"
