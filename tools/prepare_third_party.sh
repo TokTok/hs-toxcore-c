8143a6bf71eb27df87c2a169be1e3ca36c684fb3
iphydf <iphydf@users.noreply.github.com>
2022-02-13 13:06:43 +0000

    chore: Install toxcore before building haddock documentation.

---
+++ tools/prepare_third_party.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -ex
+
+sudo apt-get install libopus-dev libsodium-dev libvpx-dev
+
+git clone --depth=1 https://github.com/TokTok/c-toxcore
+cmake -Bc-toxcore/_build -Hc-toxcore -DBOOTSTRAP_DAEMON=OFF -DCMAKE_INSTALL_PREFIX:PATH="/usr"
+cmake --build c-toxcore/_build --parallel "$(nproc)"
+sudo cmake --build c-toxcore/_build --parallel "$(nproc)" --target install
+rm -rf c-toxcore
