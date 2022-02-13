26e96a10903d16aa7ce74868ad5a2aa251f91d4b
iphydf <iphydf@users.noreply.github.com>
2022-02-13 00:25:13 +0000

    cleanup: Remove the old callbacks API.
    We only support events now.

--- src/Network/Tox/C.hs
+++ src/Network/Tox/C.hs
@@ -2,8 +2,8 @@ module Network.Tox.C
   ( module M
   ) where
 
-import           Network.Tox.C.Callbacks as M
 import           Network.Tox.C.Constants as M
+import           Network.Tox.C.Events    as M
 import           Network.Tox.C.Options   as M
 import           Network.Tox.C.Tox       as M
 import           Network.Tox.C.Type      as M
--- tools/groupbot.hs
+++ tools/groupbot.hs
@@ -3,29 +3,31 @@
 {-# LANGUAGE StrictData                 #-}
 module Main (main) where
 
-import           Control.Concurrent      (threadDelay)
-import           Control.Concurrent.MVar (MVar, newMVar)
-import           Control.Exception       (AsyncException (UserInterrupt), catch,
-                                          throwIO)
-import           Control.Monad           (forever)
-import qualified Data.ByteString.Base16  as Base16
-import qualified Data.ByteString.Char8   as BS
-import           Data.String             (fromString)
-import           Data.Word               (Word32)
-import           Foreign.Storable        (Storable (..))
-import           System.Directory        (doesFileExist)
-import           System.Exit             (exitSuccess)
+import           Control.Concurrent     (threadDelay)
+import           Control.Exception      (AsyncException (UserInterrupt), catch,
+                                         throwIO)
+import           Control.Monad          (foldM)
+import qualified Data.ByteString.Base16 as Base16
+import qualified Data.ByteString.Char8  as BS
+import           Data.String            (fromString)
+import qualified Data.Text.Encoding     as Text
+import qualified Data.Text.IO           as Text
+import           Data.Word              (Word32)
+import           Foreign.Storable       (Storable (..))
+import           System.Directory       (doesFileExist)
+import           System.Exit            (exitSuccess)
 
-import qualified Network.Tox.C           as C
+import qualified Network.Tox.C          as C
+import           Network.Tox.C.Events
 
 
 bootstrapKey, masterKey :: BS.ByteString
 Right bootstrapKey =
-  Base16.decode . fromString $
-    "15E9C309CFCB79FDDF0EBA057DABB49FE15F3803B1BFF06536AE2E5BA5E4690E"
+    Base16.decode . fromString $
+        "3F0A45A268367C1BEA652F258C85F4A66DA76BCAA667A49E770BCC4917AB6A25"
 Right masterKey =
-  Base16.decode . fromString $
-    "040F75B5C8995F9525F9A8692A6C355286BBD3CF248C984560733421274F0365"
+    Base16.decode . fromString $
+        "040F75B5C8995F9525F9A8692A6C355286BBD3CF248C984560733421274F0365"
 
 isMasterKey :: BS.ByteString -> Bool
 isMasterKey = (masterKey ==)
@@ -34,24 +36,24 @@ botName :: String
 botName = "groupbot"
 
 bootstrapHost :: String
-bootstrapHost = "tox.ngc.zone"
+bootstrapHost = "tox.initramfs.io"
 
 savedataFilename :: String
 savedataFilename = "groupbot.tox"
 
 options :: BS.ByteString -> C.Options
 options savedata = C.Options
-  { C.ipv6Enabled  = True
-  , C.udpEnabled   = True
-  , C.proxyType    = C.ProxyTypeNone
-  , C.proxyHost    = ""
-  , C.proxyPort    = 0
-  , C.startPort    = 33445
-  , C.endPort      = 33545
-  , C.tcpPort      = 3128
-  , C.savedataType = if savedata == BS.empty then C.SavedataTypeNone else C.SavedataTypeToxSave
-  , C.savedataData = savedata
-  }
+    { C.ipv6Enabled  = True
+    , C.udpEnabled   = True
+    , C.proxyType    = C.ProxyTypeNone
+    , C.proxyHost    = ""
+    , C.proxyPort    = 0
+    , C.startPort    = 33445
+    , C.endPort      = 33545
+    , C.tcpPort      = 3128
+    , C.savedataType = if savedata == BS.empty then C.SavedataTypeNone else C.SavedataTypeToxSave
+    , C.savedataData = savedata
+    }
 
 
 getRight :: (MonadFail m, Show a) => Either a b -> m b
@@ -63,83 +65,86 @@ must :: Show a => IO (Either a b) -> IO b
 must = (getRight =<<)
 
 
-newtype UserData = UserData Word32
-  deriving (Eq, Storable, Read, Show)
+newtype UserData = UserData { groupNumber :: Word32 }
+    deriving (Eq, Storable, Read, Show)
 
-instance C.CHandler UserData where
-  cSelfConnectionStatus _ conn ud = do
-    putStrLn "SelfConnectionStatusCb"
-    print conn
-    return ud
+handleEvent :: C.Tox a -> UserData -> Event -> IO UserData
+handleEvent tox ud@(UserData gn) = \case
+    SelfConnectionStatus conn -> do
+        putStrLn "SelfConnectionStatusCb"
+        print conn
+        return ud
 
-  cFriendRequest tox pk msg ud = do
-    putStrLn "FriendRequestCb"
-    Right fn <- C.toxFriendAddNorequest tox pk
-    putStrLn $ (BS.unpack . Base16.encode) pk
-    putStrLn msg
-    print fn
-    return ud
+    FriendRequest (PublicKey pk) msg -> do
+        putStrLn "FriendRequestCb"
+        Right fn <- C.toxFriendAddNorequest tox pk
+        putStrLn $ (BS.unpack . Base16.encode) pk
+        Text.putStrLn $ Text.decodeUtf8 msg
+        print fn
+        return ud
 
-  cFriendConnectionStatus tox fn status ud@(UserData gn) = do
-    putStrLn "FriendConnectionStatusCb"
-    print fn
-    print status
-    if status /= C.ConnectionNone
-    then do
-      putStrLn "Inviting!"
-      _ <- C.toxConferenceInvite tox fn gn
-      return ()
-    else
-      putStrLn "Friend offline"
-    return ud
+    FriendConnectionStatus fn status -> do
+        putStrLn "FriendConnectionStatusCb"
+        print fn
+        print status
+        if status /= C.ConnectionNone
+            then do
+                putStrLn "Inviting!"
+                _ <- C.toxConferenceInvite tox fn gn
+                return ()
+            else
+                putStrLn "Friend offline"
+        return ud
 
-  cFriendMessage tox fn msgType msg ud = do
-    putStrLn "FriendMessage"
-    print fn
-    print msgType
-    putStrLn msg
-    _ <- C.toxFriendSendMessage tox fn msgType msg
-    return ud
+    FriendMessage fn msgType msg -> do
+        putStrLn "FriendMessage"
+        print fn
+        print msgType
+        Text.putStrLn $ Text.decodeUtf8 msg
+        _ <- C.toxFriendSendMessage tox fn msgType msg
+        return ud
 
-  cConferenceInvite tox fn _confType cookie ud = do
-    putStrLn "ConferenceInvite"
-    print fn
-    pk <- getRight =<< C.toxFriendGetPublicKey tox fn
-    if isMasterKey pk
-    then do
-      putStrLn "Joining!"
-      gn <- getRight =<< C.toxConferenceJoin tox fn cookie
-      return $ UserData gn
-    else do
-      putStrLn "Not master!"
-      return ud
+    ConferenceInvite fn _confType cookie -> do
+        putStrLn "ConferenceInvite"
+        print fn
+        pk <- getRight =<< C.toxFriendGetPublicKey tox fn
+        if isMasterKey pk
+            then do
+                putStrLn "Joining!"
+                newGn <- getRight =<< C.toxConferenceJoin tox fn cookie
+                return $ UserData newGn
+            else do
+                putStrLn "Not master!"
+                return ud
+
+    _ -> return ud
 
 
-loop :: MVar ud -> C.Tox ud -> IO a
-loop ud tox =
-  forever $ do
-    C.toxIterate tox ud
+loop :: C.Tox a -> UserData -> IO ()
+loop tox ud = do
     interval <- C.toxIterationInterval tox
     threadDelay $ fromIntegral $ interval * 10000
+    events <- C.toxEventsIterate tox
+    case events of
+        Left err -> fail $ show err
+        Right ok -> foldM (handleEvent tox) ud ok >>= loop tox
 
 
 main :: IO ()
 main = do
-  exists <- doesFileExist savedataFilename
-  loadedSavedata <- if exists then BS.readFile savedataFilename else return BS.empty
-  must $ C.withOptions (options loadedSavedata) $ \optPtr ->
-    must $ C.withTox optPtr $ \tox -> do
-      must $ C.toxBootstrap tox bootstrapHost 33445 bootstrapKey
+    exists <- doesFileExist savedataFilename
+    loadedSavedata <- if exists then BS.readFile savedataFilename else return BS.empty
+    must $ C.withOptions (options loadedSavedata) $ \optPtr ->
+        must $ C.withTox optPtr $ \tox -> do
+            must $ C.toxBootstrap tox bootstrapHost 33445 bootstrapKey
 
-      C.withCHandler tox $ do
-        adr <- C.toxSelfGetAddress tox
-        putStrLn $ (BS.unpack . Base16.encode) adr
-        _ <- C.toxSelfSetName tox botName
-        gn <- getRight =<< C.toxConferenceNew tox
-        ud <- newMVar (UserData gn)
-        catch (loop ud tox) $ \case
-          e@UserInterrupt -> throwIO e
-          _ -> do
-            savedSavedata <- C.toxGetSavedata tox
-            BS.writeFile savedataFilename savedSavedata
-            exitSuccess
+            adr <- C.toxSelfGetAddress tox
+            putStrLn $ (BS.unpack . Base16.encode) adr
+            _ <- C.toxSelfSetName tox botName
+            gn <- getRight =<< C.toxConferenceNew tox
+            catch (loop tox (UserData gn)) $ \case
+                e@UserInterrupt -> throwIO e
+                _ -> do
+                    savedSavedata <- C.toxGetSavedata tox
+                    BS.writeFile savedataFilename savedSavedata
+                    exitSuccess
