70943c5daecc20d7cd234fdb993a02747e16186c
iphydf <iphydf@users.noreply.github.com>
2024-02-21 19:25:14 +0000

    fix: Clone submodules when checking out toxcore.

--- tools/prepare_third_party.sh
+++ tools/prepare_third_party.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-sudo apt-get install libmsgpack-dev libopus-dev libsodium-dev libvpx-dev
+sudo apt-get install libopus-dev libsodium-dev libvpx-dev
 
-git clone --depth=1 https://github.com/TokTok/c-toxcore
+git clone --recurse-submodules --depth=1 https://github.com/TokTok/c-toxcore
 cmake -Bc-toxcore/_build -Hc-toxcore -DBOOTSTRAP_DAEMON=OFF -DCMAKE_INSTALL_PREFIX:PATH="/usr"
 cmake --build c-toxcore/_build --parallel "$(nproc)"
 sudo cmake --build c-toxcore/_build --parallel "$(nproc)" --target install
